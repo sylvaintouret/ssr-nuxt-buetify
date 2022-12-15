4f4c48e08a29bd710708c83e34d475157575bf90
sylvaintouret <sylvaintouret@gmail.com>
2022-12-15 08:58:25 +0100

    :sparkles: first commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ssr-nuxt-buetify
+
+First time trying this framework. Looks like fun
+
+## Getting Started
+
+### install nodejs 16
+
+``` bash
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&sudo apt-get install -y nodejs
+```
+
+### create new project
+
+``` yarn
+yarn create nuxt-app frontend
+```
+
+
+
---
+++ frontend/pages/cgu.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="section">
+        <div class="columns is-mobile">
+            <card
+                title="Conditions"
+                icon="text-box"
+            >
+                
+                <loremIpsum />
+            </card>
+
+        </div>
+    </section>
+</template>
+
+
+<script>
+import Card from '~/components/Card'
+import LoremIpsum from '~/components/LoremIpsum'
+export default {
+  name: 'cgu',
+  components: {
+    Card,
+    LoremIpsum
+  },
+  auth: 'guest'
+}
+
+</script>
